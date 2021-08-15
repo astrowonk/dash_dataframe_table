9ea81ee1af798b0400495fd40a733c1aa3f6eaed
Marcos Huerta <sirmarcos@gmail.com>
2021-08-15 00:16:38 -0400

    it works! needs tests and doc strings

--- dash_dataframe_table/__init__.py
+++ dash_dataframe_table/__init__.py
@@ -11,8 +11,17 @@ def _clean_header_names(x):
 def make_table_from_df(_df,
                        columns=None,
                        link_column_suffix='_HREF',
-                       cell_style_dict=None):
-    """make a dash table from a pandas dataframe but add hyperlinks based on matching column names. Conditionally style a column or columns"""
+                       cell_style_dict=None,
+                       **kwargs):
+    """make a dash table from a pandas dataframe but add hyperlinks based on matching column names. Conditionally style a column or columns
+    
+    cell_style_dict: dict of {column_name: {condition: style_dict}}
+    
+    condition can be astring or a function that returns a boolean
+
+    if condition is a string, it must match the value of the column exactly. 
+    
+    """
     if _df.empty:
         return dbc.Table()
     if columns is None:
@@ -22,28 +31,66 @@ def make_table_from_df(_df,
     col_names = list(data_dict[0].keys())
     header_column_cells = [
         html.Th(_clean_header_names(x)) for x in col_names
-        if not x.endswith('_HREF')
+        if not x.endswith(link_column_suffix)
     ]
     table_header = [html.Thead(html.Tr(header_column_cells))]
     table_body = [
-        html.Tbody(
-            [_make_row(x, col_names, link_column_suffix) for x in data_dict])
+        html.Tbody([
+            _make_row(x,
+                      col_names,
+                      link_column_suffix,
+                      cell_style_dict=cell_style_dict) for x in data_dict
+        ])
     ]
-    return dbc.Table(table_header + table_body)
+    return dbc.Table(table_header + table_body, **kwargs)
 
 
-def _make_row(data_dict_entry, col_names, link_column_suffix):
+def _make_row(
+    data_dict_entry,
+    col_names,
+    link_column_suffix,
+    cell_style_dict=None,
+):
+    if cell_style_dict is None:
+        cell_style_dict = {}
+
     def process_cell_links(col_name, link_names):
         """Add links to tables in the right way and handle nan strings."""
+        style = None
+        if cell_style_entry := cell_style_dict.get(col_name):
+            print(f"found {col_name}")
+            if isinstance(cell_style_entry[0], list):
+                print(
+                    f"found list the value {data_dict_entry[col_name]} - the  criteria {cell_style_entry[0]}"
+                )
+                if data_dict_entry[col_name] in cell_style_entry[0]:
+                    print(f"style being set to {cell_style_entry[1]}")
+                    style = cell_style_entry[1]
+            elif callable(cell_style_entry[0]):
+                if cell_style_entry[0](data_dict_entry[col_name]):
+                    style = cell_style_entry[1]
+
         if (thehref := f"{col_name}{link_column_suffix}") in link_names:
-            return dcc.Link(str(data_dict_entry[col_name]),
-                            href=str(data_dict_entry[thehref]))
+            print(data_dict_entry[thehref])
+            print(str(data_dict_entry[col_name]))
+            if data_dict_entry[thehref].startswith("http"):
+                return html.Td(html.A(
+                    str(data_dict_entry[col_name]),
+                    href=str(data_dict_entry[thehref]),
+                ),
+                               style=style)
+            return html.Td(
+                dcc.Link(
+                    str(data_dict_entry[col_name]),
+                    href=str(data_dict_entry[thehref], style=style),
+                ))
         elif isinstance(data_dict_entry[col_name], float):
             return str(int(nan_to_num(data_dict_entry[col_name])))
-        return str(data_dict_entry[col_name])
+        print(style)
+        return html.Td(str(data_dict_entry[col_name]), style=style)
 
-    link_names = [x for x in col_names if x.endswith('_HREF')]
+    link_names = [x for x in col_names if x.endswith(link_column_suffix)]
     return html.Tr([
-        html.Td(process_cell_links(x, link_names)) for x in col_names
-        if not x.endswith('_HREF')
+        process_cell_links(x, link_names) for x in col_names
+        if not x.endswith(link_column_suffix)
     ])
---
+++ example.py
@@ -0,0 +1,48 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import pandas as pd
+from dash_dataframe_table import make_table_from_df
+
+app = dash.Dash("example of dataframe",
+                external_stylesheets=[dbc.themes.YETI],
+                title="Covid Case Growth Plots",
+                meta_tags=[
+                    {
+                        "name": "viewport",
+                        "content": "width=device-width, initial-scale=1"
+                    },
+                ])
+
+the_list = [
+    "Apple", "Google", "Yahoo", "Facebook", "Microsoft", "Amazon", "IBM",
+    "Intel", "Oracle", "Intel"
+]
+## make a simple dataframe with links
+
+df = pd.DataFrame([{
+    'Company': x,
+    "Company_HREF": f"https://{x.lower()}.com",
+    "Value": n - 4
+} for n, x in enumerate(the_list)])
+
+## make the conditional styling dictionary
+my_style_dict = {
+    'Company': (['Yahoo', 'Apple'], {
+        'font-weight': 'bold'
+    }),
+    'Value': (lambda x: x > 0, {
+        'background-color': '#7FFFD4'
+    })
+}
+
+app.layout = dbc.Container([
+    html.H4('Example Table from Dataframe'),
+    ### maybe it shouldn't return a dbc table but rather the elemnts of the table so one can choose dbc or dcc
+    html.Div(
+        make_table_from_df(df, striped=True, cell_style_dict=my_style_dict))
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
