f9155d408fabf535c73754a29b4af409cba06014
Marcos Huerta <sirmarcos@gmail.com>
2021-08-15 14:22:40 -0400

    setup.py oddness

--- setup.py
+++ setup.py
@@ -102,10 +102,10 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    #packages=find_packages(
-    #     exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
-    py_modules=['dash_dataframe_table'],
+    #py_modules=['dash_dataframe_table'],
 
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
